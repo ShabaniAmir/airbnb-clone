a6d85641026c7feb3ae9aab75bee0c4bffd234e7
Amir Shabani <shabani.a.amir@gmail.com>
2023-01-30 16:41:42 -0500

    idk

--- backend/src/routes/api/index.js
+++ backend/src/routes/api/index.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 
 // GET /api/restore-user
 const { restoreUser } = require("../../utils/auth.js");
-
 router.use(restoreUser);
 
 router.get("/restore-user", (req, res) => {
