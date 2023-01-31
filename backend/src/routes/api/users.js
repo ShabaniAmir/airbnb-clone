63f52b0f82c8d45f71a1301b44698dbe9ab0d5d0
Amir Shabani <shabani.a.amir@gmail.com>
2023-01-31 18:55:46 -0500

    Get current user

--- backend/src/routes/api/users.js
+++ backend/src/routes/api/users.js
@@ -9,36 +9,50 @@ const router = express.Router();
 const validateSignup = [
   check("email")
     .exists({ checkFalsy: true })
-    .isEmail()
     .withMessage("Please provide a valid email."),
   check("firstName")
     .exists({ checkFalsy: true })
-    .withMessage("Please provide a first name."),
+    .withMessage("Please provide a firstName."),
   check("lastName")
     .exists({ checkFalsy: true })
-    .withMessage("Please provide a last name."),
+    .withMessage("Please provide a lastName."),
 
   check("username")
     .exists({ checkFalsy: true })
-    .isLength({ min: 4 })
-    .withMessage("Please provide a username with at least 4 characters."),
-  check("username").not().isEmail().withMessage("Username cannot be an email."),
+    .withMessage("Please provide a username."),
+
   check("password")
     .exists({ checkFalsy: true })
-    .isLength({ min: 6 })
-    .withMessage("Password must be 6 characters or more."),
+    .withMessage("Please provide a password."),
   handleValidationErrors,
 ];
 // Sign up
 // POST /api/users
 router.post("/", validateSignup, async (req, res) => {
-  const { email, password, username } = req.body;
-  const user = await User.signup({ email, username, password });
+  const { email, password, username, firstName, lastName } = req.body;
+  console.log({ email, password, username, firstName, lastName });
+  const user = await User.signup({
+    email,
+    username,
+    password,
+    firstName,
+    lastName,
+  });
 
   await setTokenCookie(res, user);
 
   return res.json({
     user: user,
+  });
+});
+
+// Get current user
+// GET /api/users
+router.get("/", requireAuth, async (req, res) => {
+  const { user } = req;
+  // TODO: remove unwanted fields from user object
+  return res.json({
+    user,
   });
 });
 
