5bc9d90791c130d1a74f98fc7ab2a9e19ec93c40
Amir Shabani <shabani.a.amir@gmail.com>
2023-01-31 22:46:26 -0500

    Added Review model

---
+++ backend/db/models/review.js
@@ -0,0 +1,34 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Review extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Review.belongsTo(models.User, { foreignKey: "userId" });
+      Review.belongsTo(models.Spot, { foreignKey: "spotId" });
+    }
+  }
+  Review.init(
+    {
+      userId: {
+        type: DataTypes.INTEGER,
+        references: { model: "Users" },
+      },
+      spotId: {
+        type: DataTypes.INTEGER,
+        references: { model: "Spots" },
+      },
+      stars: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "Review",
+    }
+  );
+  return Review;
+};
