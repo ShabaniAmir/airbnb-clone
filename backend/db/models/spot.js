d44921b4711c9fcc445c3b87283e2b87684b5877
Amir Shabani <shabani.a.amir@gmail.com>
2023-01-31 22:39:44 -0500

    Added Spot Model

---
+++ backend/db/models/spot.js
@@ -0,0 +1,37 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Spot extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Spot.belongsTo(models.User, { foreignKey: "ownerId" });
+    }
+  }
+  Spot.init(
+    {
+      ownerId: {
+        type: DataTypes.INTEGER,
+        references: { model: "Users" },
+      },
+      address: DataTypes.STRING,
+      city: DataTypes.STRING,
+      state: DataTypes.STRING,
+      country: DataTypes.STRING,
+      lat: DataTypes.FLOAT,
+      lng: DataTypes.FLOAT,
+      name: DataTypes.STRING,
+      description: DataTypes.STRING,
+      price: DataTypes.FLOAT,
+    },
+    {
+      sequelize,
+      modelName: "Spot",
+    }
+  );
+  return Spot;
+};
