3542e6dd108885aca641b86eb688561fbc1f00f1
Amir Shabani <shabani.a.amir@gmail.com>
2023-02-02 10:27:10 -0500

    Added user login endpoint

--- backend/src/routes/api/users.js
+++ backend/src/routes/api/users.js
@@ -58,7 +58,35 @@ router.get("/", requireAuth, async (req, res) => {
 
 // Login user
 // POST /api/users/login
-// TODO: Implement
+const validateLogin = [
+  check("credential")
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage("Please provide a valid email or username."),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a password."),
+  handleValidationErrors,
+];
+router.post("/login", validateLogin, async (req, res, next) => {
+  const { credential, password } = req.body;
+
+  const user = await User.login({ credential, password });
+
+  if (!user) {
+    const err = new Error("Login failed");
+    err.status = 401;
+    err.title = "Login failed";
+    err.errors = ["The provided credentials were invalid."];
+    return next(err);
+  }
+
+  await setTokenCookie(res, user);
+
+  return res.json({
+    user: user,
+  });
+});
 
 // Logout user
 // POST /api/users/logout
