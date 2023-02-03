e0dcf2d6fa665245d871b312d7afae226b225de7
Amir Shabani <shabani.a.amir@gmail.com>
2023-02-03 09:49:33 -0500

    Added review create endpoint

--- backend/src/routes/api/reviews.js
+++ backend/src/routes/api/reviews.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
-const { setTokenCookie, requireAuth } = require("../../utils/auth");
-const { User } = require("../../../db/models");
+const {
+  setTokenCookie,
+  requireAuth,
+  requireReviewOwnership,
+} = require("../../utils/auth");
+const { User, Spot, Review } = require("../../../db/models");
 
 const router = express.Router({ mergeParams: true });
 
@@ -12,7 +16,40 @@ const router = express.Router({ mergeParams: true });
 
 // Create a review for a spot
 // POST /api/spots/:id/reviews
-// TODO: implemenet
+const validateReview = [
+  check("review")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a review."),
+  check("stars")
+    .exists({ checkFalsy: true })
+    .isInt({ min: 1, max: 5 })
+    .withMessage("Please provide a number of stars between 1 and 5."),
+  handleValidationErrors,
+];
+router.post("/", [requireAuth, validateReview], async (req, res) => {
+  const userId = req.user.id;
+  const spotId = req.params.id;
+
+  const spot = await Spot.findByPk(spotId);
+  if (!spot) {
+    return res.status(404).json({
+      message: "Spot couldn't be found",
+      statusCode: 404,
+    });
+  }
+
+  const review = await Review.findOne({ where: { userId, spotId } });
+  if (review) {
+    return res.status(403).json({
+      message: "User already has a review for this spot",
+      statusCode: 403,
+    });
+  }
+
+  const newReview = await Review.create({ ...req.body, userId, spotId });
+
+  return res.status(201).json({ newReview });
+});
 
 // Delete a review for a spot
 // DELETE /api/spots/:id/reviews/:reviewId
